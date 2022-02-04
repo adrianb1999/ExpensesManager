7e41abcd95c9edf4b3ba90203867ed1378034b7f
adrianb1999 <blanariu_adrian@yahoo.com>
2022-02-04 12:22:28 +0200

    HTML routes added

---
+++ src/main/java/com/adrian99/expensesManager/controllers/ViewController.java
@@ -0,0 +1,62 @@
+package com.adrian99.expensesManager.controllers;
+
+import com.adrian99.expensesManager.emailVerification.TokenState;
+import com.adrian99.expensesManager.services.VerificationTokenService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ViewController {
+
+    public final VerificationTokenService verificationTokenService;
+
+    public ViewController(VerificationTokenService verificationTokenService) {
+        this.verificationTokenService = verificationTokenService;
+    }
+
+    @RequestMapping(value = {"/index.html", "/", "/index"})
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/login.html")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/createUser.html")
+    public String createAccount() {
+        return "createUser";
+    }
+
+    @RequestMapping("/user.html")
+    public String userDashboard() {
+        return "user";
+    }
+
+    @RequestMapping("/registrationConfirm.html")
+    public String confirmAccount(@RequestParam(name = "token") String token, Model model) {
+
+        TokenState tokenState = verificationTokenService.isTokenValidHtml(token);
+
+        if (!tokenState.equals(TokenState.VALID)) {
+            model.addAttribute("TokenState", tokenState);
+            return "registrationConfirm";
+        }
+        model.addAttribute("TokenState", "Token is valid!");
+        return "login";
+    }
+
+    @RequestMapping("/passwordResetForm.html")
+    public String passwordResetForm(@RequestParam(name = "token") String token, Model model) {
+        model.addAttribute("Token", token);
+        return "passwordResetForm";
+    }
+
+    @RequestMapping("/passwordReset.html")
+    public String passwordReset() {
+        return "passwordReset";
+    }
+}
