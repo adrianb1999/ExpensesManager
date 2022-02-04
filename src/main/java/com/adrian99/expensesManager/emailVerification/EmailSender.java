a64b9d90c6de0e95b23c99e9d0d0a1655648fa74
adrianb1999 <blanariu_adrian@yahoo.com>
2022-02-04 12:26:15 +0200

    Async enabled

--- src/main/java/com/adrian99/expensesManager/emailVerification/EmailSender.java
+++ src/main/java/com/adrian99/expensesManager/emailVerification/EmailSender.java
@@ -3,6 +3,7 @@ package com.adrian99.expensesManager.emailVerification;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import javax.mail.MessagingException;
@@ -14,22 +15,22 @@ public class EmailSender {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Async
     public void sendEmail(String email, String token, TokenType tokenType) throws MessagingException {
 
         MimeMessage mailMessage = mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(mailMessage,"utf-8");
+        MimeMessageHelper helper = new MimeMessageHelper(mailMessage, "utf-8");
 
         helper.setFrom("");
         helper.setTo(email);
 
         String link;
 
-        if(tokenType.equals(TokenType.ACCOUNT_ACTIVATION)) {
-            link = "Activation link: <a href=\"http://127.0.0.1:8080/registrationConfirm?token=" + token + "\" target=\"_blank\">Confirm</a>";
+        if (tokenType.equals(TokenType.ACCOUNT_ACTIVATION)) {
+            link = "Activation link: <a href=\"http://adi99.go.ro:10000/registrationConfirm.html?token=" + token + "\" target=\"_blank\">Confirm</a>";
             helper.setSubject("Expenses Manager Activation Link");
-        }
-        else {
-            link = "Password reset link: <a href=\"http://127.0.0.1:8080/passwordReset?token=" + token + "\" target=\"_blank\">Confirm</a>";
+        } else {
+            link = "Password reset link: <a href=\"http://adi99.go.ro:10000/passwordResetForm.html?token=" + token + "\" target=\"_blank\">Confirm</a>";
             helper.setSubject("Password Reset Link");
         }
 
