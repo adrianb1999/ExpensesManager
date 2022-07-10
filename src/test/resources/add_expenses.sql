a584babfb3e70c4fd985d49f36bebc8ead14ff3d
adrianb1999 <blanariu_adrian@yahoo.com>
2022-07-10 21:44:43 +0300

    Test data added

---
+++ src/test/resources/add_expenses.sql
@@ -0,0 +1,59 @@
+INSERT INTO expense(id, amount, category, date, details, pay_method, title, users_id)
+VALUES (1, 240, 'FUEL', '2021-06-25', 'Exp. no. 1', 'CARD', 'Exp. no. 1', null);
+
+INSERT INTO expense(id, amount, category, date, details, pay_method, title, users_id)
+VALUES (2, 442, 'FOOD', '2021-04-30', 'Exp. no. 2', 'CASH', 'Exp. no. 2', null);
+
+INSERT INTO expense(id, amount, category, date, details, pay_method, title, users_id)
+VALUES (3, 346, 'OTHER', '2022-01-04', 'Exp. no. 3', 'CARD', 'Exp. no. 3', null);
+
+INSERT INTO expense(id, amount, category, date, details, pay_method, title, users_id)
+VALUES (4, 165, 'SCHOOL', '2022-05-26', 'Exp. no. 4', 'CASH', 'Exp. no. 4', null);
+
+INSERT INTO expense(id, amount, category, date, details, pay_method, title, users_id)
+VALUES (5, 115, 'DRINKS', '2022-04-09', 'Exp. no. 5', 'CARD', 'Exp. no. 5', null);
+
+INSERT INTO expense(id, amount, category, date, details, pay_method, title, users_id)
+VALUES (6, 269, 'GIFT', '2021-02-11', 'Exp. no. 6', 'CARD', 'Exp. no. 6', null);
+
+INSERT INTO expense(id, amount, category, date, details, pay_method, title, users_id)
+VALUES (7, 411, 'FUEL', '2022-04-05', 'Exp. no. 7', 'CASH', 'Exp. no. 7', null);
+
+INSERT INTO expense(id, amount, category, date, details, pay_method, title, users_id)
+VALUES (8, 147, 'EDUCATION', '2021-04-05', 'Exp. no. 8', 'CASH', 'Exp. no. 8', null);
+
+INSERT INTO expense(id, amount, category, date, details, pay_method, title, users_id)
+VALUES (9, 490, 'SCHOOL', '2022-04-16', 'Exp. no. 9', 'CASH', 'Exp. no. 9', null);
+
+INSERT INTO expense(id, amount, category, date, details, pay_method, title, users_id)
+VALUES (10, 25, 'OTHER', '2021-06-02', 'Exp. no. 10', 'CARD', 'Exp. no. 10', null);
+
+INSERT INTO expense(id, amount, category, date, details, pay_method, title, users_id)
+VALUES (11, 71, 'GIFT', '2021-07-31', 'Exp. no. 11', 'CARD', 'Exp. no. 11', null);
+
+INSERT INTO expense(id, amount, category, date, details, pay_method, title, users_id)
+VALUES (12, 375, 'FOOD', '2021-06-25', 'Exp. no. 12', 'CASH', 'Exp. no. 12', null);
+
+INSERT INTO expense(id, amount, category, date, details, pay_method, title, users_id)
+VALUES (13, 259, 'CLOTHES', '2021-08-13', 'Exp. no. 13', 'CARD', 'Exp. no. 13', null);
+
+INSERT INTO expense(id, amount, category, date, details, pay_method, title, users_id)
+VALUES (14, 299, 'FOOD', '2022-05-25', 'Exp. no. 14', 'CASH', 'Exp. no. 14', null);
+
+INSERT INTO expense(id, amount, category, date, details, pay_method, title, users_id)
+VALUES (15, 153, 'BEAUTY', '2022-05-19', 'Exp. no. 15', 'CASH', 'Exp. no. 15', null);
+
+INSERT INTO expense(id, amount, category, date, details, pay_method, title, users_id)
+VALUES (16, 131, 'FOOD', '2022-04-10', 'Exp. no. 16', 'CASH', 'Exp. no. 16', null);
+
+INSERT INTO expense(id, amount, category, date, details, pay_method, title, users_id)
+VALUES (17, 52, 'DRINKS', '2021-07-08', 'Exp. no. 17', 'CASH', 'Exp. no. 17', null);
+
+INSERT INTO expense(id, amount, category, date, details, pay_method, title, users_id)
+VALUES (18, 449, 'CULTURE', '2021-07-25', 'Exp. no. 18', 'CASH', 'Exp. no. 18', null);
+
+INSERT INTO expense(id, amount, category, date, details, pay_method, title, users_id)
+VALUES (19, 287, 'CLOTHES', '2021-07-12', 'Exp. no. 19', 'CASH', 'Exp. no. 19', null);
+
+INSERT INTO expense(id, amount, category, date, details, pay_method, title, users_id)
+VALUES (20, 369, 'FOOD', '2021-01-28', 'Exp. no. 20', 'CASH', 'Exp. no. 20', null);
